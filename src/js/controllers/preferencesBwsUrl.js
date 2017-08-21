e45de107b4764ade71bf8d82415b716b2812c1bf
Nadav Ivgi <nadav@shesek.info>
2017-08-21 18:38:01 +0300

    BWS: use Bitcoin-compatible wallet service (https://bws.cobit.community/)

--- src/js/controllers/preferencesBwsUrl.js
+++ src/js/controllers/preferencesBwsUrl.js
@@ -25,11 +25,11 @@ angular.module('copayApp.controllers').controller('preferencesBwsUrlController',
       switch ($scope.bwsurl.value) {
         case 'prod':
         case 'production':
-          bws = 'https://bws.bitpay.com/bws/api'
+          bws = 'https://bws.cobit.community/bws/api'
           break;
         case 'sta':
         case 'staging':
-          bws = 'https://bws-staging.b-pay.net/bws/api'
+          bws = 'https://bws-staging.cobit.community/bws/api'
           break;
         case 'loc':
         case 'local':
